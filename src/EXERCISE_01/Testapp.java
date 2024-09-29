250479569a6ad4af030b4bc055f57f4b3f130f46
joyalmjoy <161572531+joyalmjoy@users.noreply.github.com>
2024-09-29 03:08:14 -0400

    lab 2

--- src/EXERCISE_01/Testapp.java
+++
@@ -1,9 +0,0 @@
-package EXERCISE_01;
-
-public class Testapp {
-        public static void main(String[] args) {
-            Test obj = new Test();
-            obj.inputAnswer();
-        }
-    }
-
